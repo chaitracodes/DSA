9ec455105f7dae9901b6aa308d9f6aa61c1c5f20
Chaitra S Patil <chaitrasp05@gmail.com>
2026-03-02 20:20:04 +0530

    Update longest-substring-without-repeating-characters.py

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,5 +1,14 @@
+#Given a string s, find the length of the longest substring without duplicate characters.
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+      """ Approach:
+      - Use a sliding window with two pointers.
+      - Store the last index of each character using a hashmap.
+      - Move the right pointer to expand the window.
+      - If a character repeats inside the window, jump the left pointer
+      - to one position after its previous occurrence.
+      - Update the maximum window length at each step. """
         mp = {}
         left = 0
         res = 0
@@ -11,7 +20,3 @@ class Solution:
             mp[ch]=right
             res = max(res, right - left + 1)
         return res
-
-
-
-        
