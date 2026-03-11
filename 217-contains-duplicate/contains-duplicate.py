a2cc29a82dfded36743e688607fef00c991c3603
Chaitra S Patil <chaitrasp05@gmail.com>
2026-03-11 19:42:28 +0530

    Time: 10 ms (69.73%) | Memory: 36.1 MB (18.05%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,17 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        """ Approach:
+        - Use a hashmap to track numbers already seen.
+        - Traverse the array and check if the current number exists in the hashmap.
+        - If it does, a duplicate is found.
+        - Otherwise store the number in the hashmap."""
+        mp = {}
+
+        for num in nums:
+            if num in mp:
+                return True
+            mp[num] = 1
+            
+        return False
+
+        
