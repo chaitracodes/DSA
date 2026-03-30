517914b492c1e6ab9c6cd7a4801da5753248e117
Chaitra S Patil <chaitrasp05@gmail.com>
2026-03-30 16:58:55 +0530

    Update find-all-duplicates-in-an-array.py

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
@@ -1,5 +1,11 @@
+
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
+        """Approach:
+        - Find all elements that appear twice using index marking.
+        - Treat each value as an index (value - 1).
+        - Traverse the array and mark visited indices by negating values.
+        - If an index is already negative, the number is a duplicate."""
         res = []
         for n in nums:
             n = abs(n)
